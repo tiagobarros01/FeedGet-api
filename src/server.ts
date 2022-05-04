03025d6c3d8b7672c746afb83b04536ca6a8660c
tiagobarros01 <tbarros1337@hotmail.com>
2022-05-04 10:57:16 -0300

    feat: Sends email when create a new feedback

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'ebf8e77f69911b',
+    pass: '95459db82e6468',
+  },
+});
 
 type ICreateFeedbackRequestDTO = {
   type: string;
@@ -20,6 +30,18 @@ app.post('/feedbacks', async (req: Request, res: Response) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Tiago Barros <tbarros1337@hotmail.com>',
+    subject: 'New feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   });
 
   return res.status(201).json(createdFeedback);
