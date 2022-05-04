e52a85d493a2aaf39e30a2a52696f824e0148590
tiagobarros01 <tbarros1337@hotmail.com>
2022-05-04 10:48:02 -0300

    feat: Creates a feedback route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,28 @@
-import express from "express";
+import express, { Request, Response } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.listen(3333, () => console.log('Server is running at port 3333 🔥'))
+app.use(express.json());
+
+type ICreateFeedbackRequestDTO = {
+  type: string;
+  comment: string;
+  screenshot?: string;
+};
+
+app.post('/feedbacks', async (req: Request, res: Response) => {
+  const { type, comment, screenshot } = req.body as ICreateFeedbackRequestDTO;
+
+  const createdFeedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json(createdFeedback);
+});
+
+app.listen(3333, () => console.log('Server is running at port 3333 🔥'));
