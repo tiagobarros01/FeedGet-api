1b38832151c93ee9d7ece1c54c11a4758ea18e5f
tiagobarros01 <tbarros1337@hotmail.com>
2022-05-06 09:56:04 -0300

    feaat: Adds variable port

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import cors from 'cors';
 import express from 'express';
 import { routes } from './routes';
 
+const PORT = process.env.PORT || 3333;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running at port 3333 🔥'));
+app.listen(PORT, () => console.log(`Server is running at port ${PORT} 🔥`));
